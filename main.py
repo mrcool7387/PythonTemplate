5cd163ec1e84c99a0c1063df7bdf1085cbe6a759
Alexander Schwarz <alexander.schwarz@schueler.htr-office.de>
2025-10-30 15:39:31 +0100

    Finsihed Python Template

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from _template import LOGGER, TMPDIR, TMPFILE
 
-LOGGER.exception("Running Main Module")
 
-print(TMPFILE, TMPDIR)
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    LOGGER.info("Running MainFile")
+    LOGGER.debug(__name__)
+    LOGGER.debug(__file__)
+    main()
